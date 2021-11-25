b78ebc9e5563f22a7e33fd9a374d00a8dc4adbd7
BintangRP <bintangrp45@gmail.com>
2021-11-25 20:30:27 +0700

    git init cond_ifThenElseifThenElse

---
+++ javascript/cond_ifThenElse/jadwalHarian.js
@@ -0,0 +1,20 @@
+var n = 1;
+
+if (n>5){
+    hasil = "Bangun Tidur";
+}else if(n>7){
+    hasil = "Siap-siap cool yeah";
+}else if(n>11){
+    hasil = "Jemput ibu";
+}else if(n>15){
+    hasil = "Rebahan";
+}else if(n>18){
+    hasil = "Istirahat";
+}else if(n>20){
+    hasil = "Belajar bentar kuy";
+}else if(n>23){
+    hasil = "Istirahat again";
+}else{
+    hasil = "Teparrr";
+}
+console.log(hasil);
