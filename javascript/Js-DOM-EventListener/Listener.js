5a993cc8fdd569d4e8a4fb08d1cc49fa4ccbe91f
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2021-12-13 02:25:35 +0700

    JS DOM EventListeners new

---
+++ javascript/Js-DOM-EventListener/Listener.js
@@ -0,0 +1,19 @@
+// button
+let btn = document.getElementsByTagName('button')[0];
+
+btn.addEventListener("click", Clicked);//add event listener
+
+function Clicked(){ 
+    //elemen baru P
+    let newInput = document.createElement("div")
+
+    //memasukan inputan text
+    let data1 = newInput.innerHTML = document.getElementById('nama').value
+    let data2 = newInput.innerHTML = document.getElementById('gender').value
+    let data3 = newInput.innerHTML = document.getElementById('age').value
+
+    let Data = data1 + data2 + data3
+    let biodata = document.body.append(Data)
+    console.log(biodata)
+    
+}
